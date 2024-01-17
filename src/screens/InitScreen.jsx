27de451efbac38923c717c1cbf1f700dddb2f96d
Rodrigo Gómez <loroxeraco@hotmail.com>
2024-01-17 12:23:41 +0100

    Show favved airports on init screen

--- src/screens/InitScreen.jsx
+++ src/screens/InitScreen.jsx
@@ -1,28 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import theme from '../theme';
 import SearchInputLabel from '../components/SearchInputLabel';
 
+
 const InitScreen = () => {
-    return (
+  const [favAirportList, setFavAirportList] = useState([]);
+
+  useEffect(() => {
+    retrieveStoredFavAirports();
+  }, []);
+
+  const retrieveStoredFavAirports = async () => {
+    try {
+      let favAirportStorage = await AsyncStorage.getItem('userFavAirports');
+      
+      // If favAirportStorage is null, return an empty array
+      let favAirportList = JSON.parse(favAirportStorage);
+
+      setFavAirportList(favAirportList.sort((a, b) => a.localeCompare(b)));
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+
+
+  return (
         <View style={styles.container}>
             <SearchInputLabel />
+            {favAirportList ? favAirportList.map((airport) => <Text key={airport}>{airport}</Text>) : <Text>Loading...</Text>}
         </View>
     );
 }
 
-const getFavAirports = async () => {
-    try {
-      const value = await AsyncStorage.getItem('fav_airports');
-      if (value !== null) {
-        // value previously stored
-      }
-    } catch (e) {
-      // error reading value
-    }
-  };
 
 const styles = StyleSheet.create({
     container: {
