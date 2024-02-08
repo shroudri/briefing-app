4b5996197bf394e36f0354a69557dc55dbd5d533
Rodrigo Gómez <loroxeraco@hotmail.com>
2024-02-08 16:57:39 +0100

    Create separate component to delete all favorites

---
+++ src/components/ButtonDeleteFavAirports.jsx
@@ -0,0 +1,52 @@
+import { View, Text, StyleSheet, ScrollView, TouchableOpacity, Alert } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import theme from '../theme';
+
+export default function ButtonDeleteFavAirports(props) {
+    const deleteFavAirportList = async () => {
+        try {
+            await AsyncStorage.removeItem('userFavAirports');
+        } catch (error) {
+            console.error('Error deleting data:', error);
+        }
+    }
+
+    const handleStorageDeletionPress = () =>
+        Alert.alert(
+            'Caution',
+            'Are you sure that you want to erase your list of favorites?',
+            [
+                { text: 'Cancel', style: 'cancel',},
+                { text: 'Delete', onPress: () => {deleteFavAirportList(), props.setFavAirportList([])},},
+            ],
+            {
+                cancelable: true,
+            },
+        )
+
+    return (
+        <View>
+            <TouchableOpacity onPress={handleStorageDeletionPress} style={privStyles.deleteListButton}>
+                <Text style={{ color: "#d23c34" }}>Delete all favorites</Text>
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+
+
+
+const privStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: theme.colors.background
+    },
+    deleteListButton: {
+        margin: 15,
+        padding: 10,
+        backgroundColor: "#e5e5e5",
+        borderRadius: 5,
+        borderWidth: 1
+    }
+})
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,17 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, TouchableOpacity, Alert } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import theme from '../theme';
 import SearchBar from '../components/SearchBar';
 import AirportQuickCard from '../components/AirportQuickCard';
+import ButtonDeleteFavAirports from '../components/ButtonDeleteFavAirports';
 
 const HomeScreen = () => {
   const [favAirportList, setFavAirportList] = useState([]);
-
-  useEffect(() => {
-    retrieveStoredFavAirports();
-  }, []);
 
   const retrieveStoredFavAirports = async () => {
     try {
@@ -22,28 +19,43 @@ const HomeScreen = () => {
 
       setFavAirportList(favAirportList.sort((a, b) => a.localeCompare(b)));
     } catch (error) {
-      console.error('Error fetching data:', error);
+      console.log('Unable to read userFavAirports from local Storage. Maybe its empty?:', error);
     }
   };
 
+  useEffect(() => {
+    retrieveStoredFavAirports();
+  }, []);
+
   return (
-        <View style={styles.container}>
-            <SearchBar/>
-            <ScrollView>
-              {favAirportList
-               ? favAirportList.map((airport) => 
-                  <AirportQuickCard key={airport} airport={airport} />) 
-               : <Text>Loading...</Text>}
-            </ScrollView>
-        </View>
+    <View style={privStyles.container}>
+      <SearchBar />
+      <ScrollView>
+        {favAirportList
+          ? favAirportList.map((airport) =>
+            <AirportQuickCard key={airport} airport={airport} />)
+          : <Text>Loading...</Text>}
+
+        {favAirportList.length === 0
+          ? <Text>No favorites yet</Text>
+          : <ButtonDeleteFavAirports setFavAirportList={setFavAirportList}/>}
+      </ScrollView>
+    </View>
     );
 }
 
 
-const styles = StyleSheet.create({
+const privStyles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: theme.colors.background
+    },
+    deleteListButton: {
+        margin: 15,
+        padding: 10,
+        backgroundColor: "#e5e5e5",
+        borderRadius: 5,
+        borderWidth: 1
     }
 })
 
