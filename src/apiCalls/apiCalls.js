e8e6934c18f8f1f8064fc84f2785d8e590d20c02
Rodrigo Gómez <loroxeraco@hotmail.com>
2024-02-18 22:59:58 +0100

    Change apiCalls from HTTP to HTTPS

--- src/apiCalls/apiCalls.js
+++ src/apiCalls/apiCalls.js
@@ -1,6 +1,6 @@
 export async function fetchMetars(airport) {
   console.log("Fetching METARS for: " + airport);
-  const url = `http://aviationweather.gov/api/data/metar?ids=${airport}&format=json&hours=6`
+  const url = `https://aviationweather.gov/api/data/metar?ids=${airport}&format=json&hours=6`
   const response = await fetch(url);
   const data = await response.json();
   // console.log(data);
